87ff6d78f99f767ded9d0980cae4b8b3f446111c
“[firstname <[valid-email]>
2026-02-18 00:19:56 +0530

    chat

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -1,1 +1,21 @@
-console.log("start th code");
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+ 
+let userCount = 0;
+
+ let allSocket: WebSocket[] = [];
+ wss.on("connection",(socket) =>{
+
+    allSocket.push(socket);
+    userCount = userCount +1;
+    console.log("user Connected #" + userCount);
+      socket.on("message", (message) =>{
+        console.log("message recieved  " + message.toString());
+
+        allSocket.forEach(s =>{
+            s.send(message.toString() + ": its from server");
+        })
+      })
+    
+ })
